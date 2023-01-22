80f9f8af121738d1ad91b0279ca585a3c0792a52
Shivam Seth <72886216+ShivamSeth05@users.noreply.github.com>
2023-01-22 23:33:38 +0530

    Update Products.java

--- setProducts/Products.java
+++ setProducts/Products.java
@@ -1,52 +1,22 @@
 package setProducts;
 
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
 
 public class Products {
 
-    public static void main(String[] args) {
-        Set<String> products = new HashSet<>();
-        @SuppressWarnings("resource")
-		Scanner sc = new Scanner(System.in);
+		public String code,name;
+		public float price;
+		public int qty;
 
-        // Adding products to the set
-        System.out.println("Enter the name of the products you want to add:");
-        while (sc.hasNextLine()) {
-            String product = sc.nextLine();
-            if (product.equals("")) {
-                break;
-            }
-            products.add(product);
-        }
-
-        System.out.println("Current products in the set: " + products);
-
-        // Removing a product from the set
-        System.out.println("Enter the name of the product you want to remove:");
-        String productToRemove = sc.nextLine();
-        products.remove(productToRemove);
-        System.out.println("Current products in the set: " + products);
-
-        // Union
-        Set<String> newProducts = new HashSet<>();
-        newProducts.add("Laptop");
-        newProducts.add("Tablet");
-        newProducts.add("Smartphone");
-        newProducts.add("Television");
-        Set<String> union = new HashSet<>(products);
-        union.addAll(newProducts);
-        System.out.println("Union of products: " + union);
-
-        // Intersection
-        Set<String> intersection = new HashSet<>(products);
-        intersection.retainAll(newProducts);
-        System.out.println("Intersection of products: " + intersection);
-
-        // Difference
-        Set<String> difference = new HashSet<>(products);
-        difference.removeAll(newProducts);
-        System.out.println("Difference of products: " + difference);
-    }
+		public Products(String code,String name,float
+				price,int qty)
+		{
+			this.code=code;
+			this.name=name;
+			this.price=price;
+			this.qty=qty;
+		}
+		public String toString() {
+			return code+"\t"+name+"\t"+price+"\t"+qty;
+		}
+	
 }
